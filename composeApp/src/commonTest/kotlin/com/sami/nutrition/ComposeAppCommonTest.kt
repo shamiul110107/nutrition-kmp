bb166f55f06fb004504d405e95781a8247186d6f
Md. Shamiul Islam <shamiulcse.pust@gmail.com>
2025-08-05 10:21:27 +0700

    Initial commit

---
+++ composeApp/src/commonTest/kotlin/com/sami/nutrition/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.sami.nutrition
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/sami/nutrition/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.sami.nutrition
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
