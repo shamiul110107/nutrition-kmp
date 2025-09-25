23552cc21ca8934a3873e384f3ede3f9c877aeb7
shamiul <shamiul.islam@7peakssoftware.com>
2025-09-25 10:27:46 +0700

    added product overview screen

---
+++ feature/home/products_overview/build.gradle.kts
@@ -0,0 +1,68 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    listOf(
+        //iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "products_overview"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+            implementation(libs.compose.navigation)
+            implementation(libs.messagebar.kmp)
+
+            implementation(libs.coil3)
+            implementation(libs.coil3.compose)
+            implementation(libs.coil3.compose.core)
+            implementation(libs.coil3.network.ktor)
+
+            implementation(project(path = ":shared"))
+            implementation(project(path = ":data"))
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.darwin.client)
+        }
+    }
+}
+
+android {
+    namespace = "com.sami.products_overview"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
---
+++ feature/home/products_overview/src/commonMain/kotlin/com/nutrition/products_overview/ProductsOverviewViewModel.kt
@@ -0,0 +1,32 @@
+package com.nutrisport.products_overview
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nutrisport.data.domain.ProductRepository
+import com.nutrisport.shared.util.RequestState
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+
+class ProductsOverviewViewModel(
+    private val productRepository: ProductRepository,
+) : ViewModel() {
+    val products = combine(
+        productRepository.readNewProducts(),
+        productRepository.readDiscountedProducts()
+    ) { new, discounted ->
+        when {
+            new.isSuccess() && discounted.isSuccess() -> {
+                RequestState.Success(new.getSuccessData() + discounted.getSuccessData())
+            }
+
+            new.isError() -> new
+            discounted.isError() -> discounted
+            else -> RequestState.Loading
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = RequestState.Loading
+    )
+}
